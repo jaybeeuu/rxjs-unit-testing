b1086a14a02b2adbd2071a8c83d09e1a8d856809
Josh Bickley-Wallace <joshbickleywallace@outlook.com>
2022-11-08 21:00:39 +0000

    Make alphabet invasion scrutible.

--- src/alphabet-invasion.ts
+++ src/alphabet-invasion.ts
@@ -1,14 +1,13 @@
-// RxJS v6+
 import { interval, fromEvent, combineLatest, BehaviorSubject } from "rxjs";
 import { scan, startWith, map, takeWhile, switchMap } from "rxjs/operators";
 
 export interface Letter {
-  letter: String;
+  letter: string;
   yPos: number;
 }
 export interface Letters {
-  ltrs: Letter[];
-  intrvl: number;
+  letters: Letter[];
+  interval: number;
 }
 export interface State {
   score: number;
@@ -16,71 +15,71 @@ export interface State {
   level: number;
 }
 
-const randomLetter = () =>
-  String.fromCharCode(
-    Math.random() * ("z".charCodeAt(0) - "a".charCodeAt(0)) + "a".charCodeAt(0)
-  );
 const levelChangeThreshold = 20;
 const speedAdjust = 50;
 const endThreshold = 15;
 const gameWidth = 30;
-
 const intervalSubject = new BehaviorSubject(600);
 
-const letters$ = intervalSubject.pipe(
-  switchMap(i =>
-    interval(i).pipe(
-      scan<number, Letters>
-      (letters => ({
-        intrvl: i,
-        ltrs: [
-          {
-            letter: randomLetter(),
-            yPos: Math.floor(Math.random() * gameWidth)
-          },
-          ...letters.ltrs
-        ]
-      }),
-      { ltrs: [], intrvl: 0 })
-    )
-  )
+const randomLetter = (): string => String.fromCharCode(
+  Math.random() * ("z".charCodeAt(0) - "a".charCodeAt(0)) + "a".charCodeAt(0));
+
+const letterState$ = intervalSubject.pipe(
+  switchMap(i => interval(i)
+    .pipe(
+      scan<number, Letters>((letters) => ({
+        interval: i,
+        letters: [({
+          letter: randomLetter(),
+          yPos: Math.floor(Math.random() * gameWidth)
+        }), ...letters.letters]
+      }), { letters: [], interval: 0 })
+    )));
+
+const keys$ = fromEvent<KeyboardEvent>(document, "keydown")
+  .pipe(
+    startWith({ key: "" }),
+    map((e: { key: string }) => e.key)
+  );
+
+const game$ = combineLatest([keys$, letterState$]).pipe(
+  scan<[string, Letters], State>(
+    (state, [key, latterState]) => {
+      const { letter } = latterState.letters.at(-1) ?? {};
+
+      if (letter && letter === key)
+      {
+        state.score = state.score + 1;
+        latterState.letters.pop();
+      }
+
+      if (state.score > 0 && state.score % levelChangeThreshold === 0) {
+        latterState.letters = [];
+        state.level = state.level + 1;
+        state.score = state.score + 1;
+        intervalSubject.next(latterState.interval - speedAdjust);
+      }
+
+      return { score: state.score, letters: latterState.letters, level: state.level };
+    },
+    { score: 0, letters: [], level: 1 }),
+  takeWhile(state => state.letters.length < endThreshold),
 );
 
-const keys$ = fromEvent(document, "keydown").pipe(
-  startWith({ key: "" }),
-  map((e: KeyboardEvent) => e.key)
-);
+const appendToDocumentHtml = (html: string): void => {
+  document.body.innerHTML += html;
+};
+const renderGame = (state: State): void => {
+  document.body.innerHTML = `Score: ${state.score}, Level: ${state.level} <br/>`,
+  state.letters.forEach(l => document.body.innerHTML
+    = `${document.body.innerHTML}${"&nbsp".repeat(l.yPos)}${l.letter}<br/>`),
+  appendToDocumentHtml(
+    `${"<br/>".repeat(endThreshold - state.letters.length - 1)}${"-".repeat(gameWidth)}`
+  );
+};
+const renderGameOver = (): void => appendToDocumentHtml("<br/>GAME OVER!");
 
-const renderGame = (state: State) => (
-  (document.body.innerHTML = `Score: ${state.score}, Level: ${state.level} <br/>`),
-  state.letters.forEach(
-    l =>
-      (document.body.innerHTML += "&nbsp".repeat(l.yPos) + l.letter + "<br/>")
-  ),
-  (document.body.innerHTML
-    += "<br/>".repeat(endThreshold - state.letters.length - 1)
-    + "-".repeat(gameWidth))
-);
-const renderGameOver = () => (document.body.innerHTML += "<br/>GAME OVER!");
-const noop = () => {};
-
-const game$ = combineLatest(keys$, letters$).pipe(
-  scan<[string, Letters], State>
-  ((state, [key, letters]) => (
-    letters.ltrs[letters.ltrs.length - 1]
-      && letters.ltrs[letters.ltrs.length - 1].letter === key
-      ? ((state.score = state.score + 1), letters.ltrs.pop())
-      : noop,
-    state.score > 0 && state.score % levelChangeThreshold === 0
-      ? ((letters.ltrs = []),
-      (state.level = state.level + 1),
-      (state.score = state.score + 1),
-      intervalSubject.next(letters.intrvl - speedAdjust))
-      : noop,
-    { score: state.score, letters: letters.ltrs, level: state.level }
-  ),
-  { score: 0, letters: [], level: 1 }),
-  takeWhile(state => state.letters.length < endThreshold)
-);
-
-game$.subscribe(renderGame, noop, renderGameOver);
+game$.subscribe({
+  next: renderGame,
+  complete: renderGameOver
+});
